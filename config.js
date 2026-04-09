44d332716932fb36b8ddb8a31618ef771bb185e8
vivaca86 <vivaca86@users.noreply.github.com>
2026-04-09 21:23:27 +0900

    Configure public Apps Script gateway

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-window.STOCK_LAB_CONFIG = {
-    gatewayUrl: ""
+﻿window.STOCK_LAB_CONFIG = {
+    gatewayUrl: "https://script.google.com/macros/s/AKfycbxcQXuOXziWYz7eltmG3dN6Pvu3fJWD2S1zakTehM_vA2ubyyIxLelEe6dneyWZi3H2/exec"
 };
+
