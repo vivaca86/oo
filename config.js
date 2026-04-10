50f50f7421cedfc7c01f885858a6f0a82bd0454c
vivaca86 <vivaca86@gmail.com>
2026-04-10 10:42:48 +0900

    Expose connected sheet metadata in gateway health response

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.STOCK_LAB_CONFIG = {
-    gatewayUrl: "https://script.google.com/macros/s/AKfycbxcQXuOXziWYz7eltmG3dN6Pvu3fJWD2S1zakTehM_vA2ubyyIxLelEe6dneyWZi3H2/exec",
-    realtimeUrl: ""
+    gatewayUrl: "https://script.google.com/macros/s/AKfycbwY8HeytMMA6kmlihyrwLYBPKt4g-4Sb9btRSLcz3FQhSl2WFdZjfgCr2UCJ3NIgrg0iw/exec",
+    realtimeUrl: "https://oo-l347.onrender.com/stream"
 };
